e987d30648528da092c5bd302f671c0b082a6fd2
Al Ameen S <alameensameen77@gmail.com>
2025-08-15 14:33:48 +0530

    Sesssion and route handled

--- app.js
+++ app.js
@@ -6,98 +6,97 @@ const session = require("express-session");
 const nocache = require("nocache");
 const ejs = require('ejs');
 
-// env variables
-
 const PORT = process.env.PORT;
 const LOGINUSER = process.env.LOGINUSER;
 const PASSWORD = process.env.PASSWORD;
 const SECRET = process.env.SECRET;
+const ROLE = process.env.ROLE;
 
-// setting view engine and static files
+let letterSpace;
+let invalid;
 
-app.set('view engine' , 'ejs');
+
+app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-// middleware for understanding data
 
 app.use(express.json());
-app.use(express.urlencoded({ extended : true }));
+app.use(express.urlencoded({ extended: true }));
 
-// no cache middleware
 
 app.use(nocache());
 
-// session handling middleware
 
 app.use(session({
     name: 'ameen.sid',
-    secret : SECRET,
-    resave : false,
-    saveUninitialized : false,
-    // cookie : { maxAge : 5000 }
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie : { maxAge : 60000 }
 }))
 
-// rendering route - home
 
-app.get('/' , (req,res)=>{
-    if(req.session.admin == LOGINUSER){
+app.get('/', (req, res) => {
+    if (req.session.admin && req.session.role == ROLE) {
+        console.log(req.session);
         res.render("home");
-    }else{
-        if(req.session.letterSpace){
-            req.session.letterSpace = false;
-            return res.render("login" , { msg : "User name must be letter"})
-        }
-        if(req.session.invalid){
-            req.session.invalid = false;
-            return res.render('login' , { msg : "Invalid Credentials"})
-        }
+    } else {
         res.redirect("/login");
     }
 })
 
-// rendering route - login
 
-app.get('/login', (req,res)=>{
-    if(!req.session.admin){
-        res.render('login',{ msg : null});
-    }else{
-        res.redirect('/')
+app.get('/login', (req, res) => {
+    if (!req.session.admin) {
+        console.log(req.session);
+        
+        if (letterSpace) {
+            letterSpace = false;
+            return res.render("login", { msg: "User name must be letter" })
+        }
+        if (invalid) {
+            invalid = false;
+            return res.render('login', { msg: "Invalid Credentials" })
+        }
+        if (req.session.unauth) {
+            req.session.unauth = false;
+            return res.render('login', { msg: "Unauthorized" });
+        }
+        res.render('login', { msg: null })
+    } else {
+        res.redirect('/');
     }
+
 })
 
-// form validating route - post
 
-app.post("/verify" , (req,res)=>{
-    if((req.body.username).trim() == "" || /\d/.test(req.body.username)){
-        req.session.letterSpace = true;
-        return res.redirect('/')
-    }
-    if(req.body.username != LOGINUSER || req.body.password != PASSWORD){
-        req.session.invalid = true;
-        return res.redirect('/');
+app.post("/verify", (req, res) => {
+    if ((req.body.username).trim() == "" || /\d/.test(req.body.username)) {
+        letterSpace = true;
+        return res.redirect('/login')
     }
 
-    if(req.body.username === LOGINUSER && req.body.password === PASSWORD){
+    if (req.body.username === LOGINUSER && req.body.password === PASSWORD) {
         req.session.admin = LOGINUSER;
+        req.session.role = "admin";
+        req.session.unauth = false;
         return res.redirect('/')
+    } else {
+        invalid = true;
+        return res.redirect('/login');
     }
 })
 
-// logout route
-
-app.post('/logout' , (req,res)=>{
+app.post('/logout', (req, res) => {
     req.session.destroy();
     res.clearCookie('ameen.sid')
     res.redirect("/login");
 })
 
-// error page handling middleware
-
-app.use((req,res)=>{
+app.use((req, res) => {
     res.status(404).render('error');
 })
 
-app.listen(PORT , ()=>{
-    console.log("Server running on port " , PORT);
+app.listen(PORT, () => {
+    console.log("Server running on port ", PORT);
 })
-
